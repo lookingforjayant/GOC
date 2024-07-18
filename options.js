e25515ed0d189b4300816cd2cf3ca1338765352d
lookingforjayant <163731539+lookingforjayant@users.noreply.github.com>
2024-07-18 11:56:26 +0530

    Updated

--- options.js
+++ options.js
@@ -1,17 +1,14 @@
-// Saves options to chrome.storage
-const saveOptions = () => {
-    //const color = document.getElementById('color').value;
-    //const likesColor = document.getElementById('like').checked;
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
+
+function saveOptions() {
     const element = document.getElementById('element').value;
     const attribute = document.getElementById('attribute').value;
     const rule = document.getElementById('rule').value;
 
-
     chrome.storage.sync.set(
-        //{ favoriteColor: color, likesColor: likesColor },
         { element: element, attribute: attribute, rule: rule },
         () => {
-            // Update status to let user know options were saved.
             const status = document.getElementById('status');
             status.textContent = 'Options saved.';
             setTimeout(() => {
@@ -24,15 +21,11 @@ const saveOptions = () => {
     );
 };
 
-// Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-const restoreOptions = () => {
+function restoreOptions() {
     chrome.storage.sync.get(
-        //{ favoriteColor: 'red', likesColor: true },
         { element: element, attribute: attribute, rule: rule },
         (items) => {
-            //document.getElementById('color').value = items.favoriteColor;
-            //document.getElementById('like').checked = items.likesColor;
             document.getElementById('element').value = items.element;
             document.getElementById('attribute').value = items.attribute;
             document.getElementById('rule').value = items.rule;
@@ -40,5 +33,3 @@ const restoreOptions = () => {
     );
 };
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('save').addEventListener('click', saveOptions);
